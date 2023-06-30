d52d6ae17fa2b0f1dd00e0f468707962c505a7e3
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-06-29 21:40:06 -0300

    feat: Actions CRUD

--- backend/app/Http/Controllers/ActionController.php
+++ backend/app/Http/Controllers/ActionController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdateActionRequest;
 use App\Http\Resources\ActionsResource;
 use App\Models\Action;
 use App\Models\Company;
 use App\Models\Goal;
+use App\Models\Responsible;
+use App\Traits\HttpResponses;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ActionController extends Controller
 {
+    use HttpResponses;
+
     /**
      * Display a listing of the resource.
      */
@@ -21,19 +27,40 @@ class ActionController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUpdateActionRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        try {
+
+            Goal::findOrFail($request->goal_id);
+
+        } catch(ModelNotFoundException $exception) {
+
+            $message = 'Esta meta não existe.';
+
+            return response()->json(['message' => $message], 404);
+
+        }
+
+        try {
+
+            Responsible::findOrFail($request->responsible_id);
+
+        } catch(ModelNotFoundException $exception) {
+
+            $message = 'Este responsável não existe.';
+
+            return response()->json(['message' => $message], 404);
+
+        }
+
+
+        $action = Action::create($data);
+
+        return new ActionsResource($action);
     }
 
     /**
@@ -49,7 +76,7 @@ class ActionController extends Controller
 
             $message = 'Esta ação não existe.';
 
-            return response()->json(['message' => $message, 404]);
+            return response()->json(['message' => $message], 404);
 
         }
 
@@ -68,7 +95,7 @@ class ActionController extends Controller
 
             $message = 'Esta meta não existe.';
 
-            return response()->json(['message' => $message, 404]);
+            return response()->json(['message' => $message], 404);
 
         }
 
@@ -76,19 +103,51 @@ class ActionController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StoreUpdateActionRequest $request, string $id)
     {
-        //
+        $data = $request->validated();
+
+        try {
+
+            $action = Action::findOrFail($id);
+
+        } catch(ModelNotFoundException $exception) {
+
+            $message = 'Esta ação não existe.';
+
+            return response()->json(['message' => $message], 404);
+
+        }
+
+        try {
+
+            Goal::findOrFail($request->goal_id);
+
+        } catch(ModelNotFoundException $exception) {
+
+            $message = 'Esta meta não existe.';
+
+            return response()->json(['message' => $message], 404);
+
+        }
+
+        try {
+
+            Responsible::findOrFail($request->responsible_id);
+
+        } catch(ModelNotFoundException $exception) {
+
+            $message = 'Este responsável não existe.';
+
+            return response()->json(['message' => $message], 404);
+
+        }
+
+        $action->update($data);
+
+        return new ActionsResource($action);
     }
 
     /**
@@ -96,7 +155,21 @@ class ActionController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+
+            $action = Action::findOrFail($id);
+
+        } catch(ModelNotFoundException $exception) {
+
+            $message = 'Esta ação não existe';
+
+            return response()->json(['message' => $message], 404);
+
+        }
+
+        $action->delete();
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 
 }
---
+++ backend/app/Http/Requests/StoreUpdateActionRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreUpdateActionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+
+        $rules = [
+            'goal_id' => 'required',
+            'responsible_id' => 'required',
+            'name' => 'required|min:5|max:300|unique:actions',
+            'area' => 'required',
+            'what' => 'required',
+            'how' => 'required',
+            'start_at' => 'required',
+            'end_at' => 'required',
+            'value' => 'required',
+            'value_status' => 'required',
+            'status' => 'required',
+            'priority' => 'required',
+            'observation' => 'nullable|max:300',
+        ];
+
+        if($this->method() === 'PATCH') {
+
+            $rules['name'] = [
+                'required',
+                'min:5',
+                'max:300',
+                Rule::unique('actions')->ignore($this->id)
+            ];
+
+        }
+
+        return $rules;
+    }
+    public function messages()
+    {
+        return [
+            'goal_id.required' => 'O ID da empresa deve ser preenchido.',
+            'responsible_id.required' => 'O campo nome é obrigatório.',
+            'name.unique' => 'Este nome já existe.',
+            'name.min' => 'O campo nome deve possuir pelo 5 caracteres.',
+            'area.required' => 'O campo área é obrigatório.',
+            'what.required' => 'O campo "O que fazer" é obrigatório.',
+            'how' => 'O campo "Como fazer" é obrigatório.',
+            'start_at' => 'O campo Previsão de início é obrigatório.',
+            'end_at' => 'O campo Previsão de término é obrigatório.',
+            'value' => 'O campo Preço é obrigatório.',
+            'value_status' => 'O campo Situação do Preço é obrigatório.',
+            'status' => 'O campo status é obrigatório.',
+            'priority' => 'O campo prioridade é obrigatório.',
+        ];
+    }
+}
