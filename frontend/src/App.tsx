5d429d87f60c1e0975164805260ec885fc58d2cd
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-05 17:47:55 -0300

    feat: Company store route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Menu from '@components/Menu';
+
 import Companies from '@pages/Companies';
+import StoreCompany from '@pages/Companies/Store';
 import Dashboard from '@pages/Dashboard';
 import Error404 from '@pages/Error/404';
 import Login from '@pages/Login';
@@ -12,30 +15,54 @@ import Login from '@pages/Login';
 function App() {
 
   return (
-    <div className="App">
-      Application
+    <div className="flex flex-row h-full w-full">
+      <Menu />
+
       <Routes>
+        <Route
+          path="/"
+          element={
+            <RequireAuth loginPath="/login">
+              <Login />
+            </RequireAuth>
+          }
+        />
 
-        <Route path='/login' element={<Login />} />  
+        <Route path="/login" element={<Login />} errorElement={<Error404 />} />
 
-        <Route path='/dasboard' element={
-            <RequireAuth loginPath='/login'>
+        <Route
+          path="/dasboard"
+          element={
+            <RequireAuth loginPath="/login">
               <Dashboard />
             </RequireAuth>
-          } 
-        />  
-        <Route path='/companies' element={
-            <RequireAuth loginPath='/login'>
+          }
+          errorElement={<Error404 />}
+        />
+
+        <Route
+          path="/companies"
+          element={
+            <RequireAuth loginPath="/login">
               <Companies />
             </RequireAuth>
           }
-        />  
-        
-      </Routes>      
+          errorElement={<Error404 />}
+        />
+
+        <Route
+          path="/companies/store"
+          element={
+            <RequireAuth loginPath="/login">
+              <StoreCompany />
+            </RequireAuth>
+          }
+        />
+      </Routes>
 
       <ToastContainer />
     </div>
-  )
+  );
 }
 
 export default App
