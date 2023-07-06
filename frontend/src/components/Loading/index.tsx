3862b2dd435085fb85f968fbf5503c76bd73c2df
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-05 22:36:06 -0300

    feat: loading component

---
+++ frontend/src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import LoopIcon from '@mui/icons-material/Loop';
+
+const Loading = () => {
+    return (
+        <div className="px-3 py-2">
+            <LoopIcon className="animate-spin text-2xl fill-white" />
+        </div>
+    )
+}
+
+export default Loading
