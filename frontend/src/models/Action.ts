ed3a333fe9a41321e48c0b412ab17e3cf905fc7a
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-07 21:07:51 -0300

    types: Action type

---
+++ frontend/src/models/Action.ts
@@ -0,0 +1,23 @@
+export type ActionType = {
+    id: number;
+    attributes: ActionAttributes; 
+};
+
+export type ActionAttributes = {
+    name: string;
+    area: string;
+    what: string;
+    how: string;
+    start_at: string;
+    end_at: string;
+    remaining_days: number;
+    value: string;
+    value_status: 'Solicitar Orçamento' | 'Aguardando Cotação' | 'Orçamento em Apreciação' | 'Aprovado' | 'Não Aprovado' | 'Sem ônus' | 'Não definido';
+    status: 'A Iniciar' | 'Em Andamento' | 'Finalizado';
+    priority: 'Baixa' | 'Média' | 'Alta';
+    observation: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export default ActionType;
