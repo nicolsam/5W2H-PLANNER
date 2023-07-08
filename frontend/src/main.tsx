de518190412d495973e07eabfe166b5d7fcdf67a
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-07 21:09:45 -0300

    feat: new button variants and localization provider for Datepicker component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,65 +8,87 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { AuthProvider } from 'react-auth-kit';
 
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/en-gb';
+
 import { GlobalProvider } from '@contexts/Provider';
 
 import App from './App';
 
 const theme = createTheme({
+  palette: {
+    text: {
+      disabled: '#444344',
+    },
+  },
   components: {
     MuiButton: {
       variants: [
         {
-          props: { variant: "list" },
+          props: { variant: 'list' },
           style: {
             backgroundColor: '#444344',
             justifyContent: 'space-between',
-            padding: "1rem",
-            zIndex: "10"
-          }
+            padding: '1rem',
+            zIndex: '10',
+          },
         },
         {
-          props: { variant: "main" },
+          props: { variant: 'main' },
           style: {
             backgroundColor: '#585858',
             color: 'white',
-            padding: "1rem",
-            "&:hover": {
+            padding: '1rem',
+            '&:hover': {
               backgroundColor: '#444344',
-            }
-          }
+            },
+          },
         },
         {
-          props: { variant: "secondary" },
+          props: { variant: 'secondary' },
           style: {
             backgroundColor: '#585858',
             color: 'white',
-            padding: ".5rem 1rem",
-            "&:hover": {
+            padding: '.5rem 1rem',
+            '&:hover': {
               backgroundColor: '#444344',
-            }
-          }
+            },
+          },
         },
-      ]
-    }
-  }
+        {
+          props: { variant: 'action' },
+          style: {
+            backgroundColor: '#444344',
+            color: 'white',
+            padding: '.7rem',
+            '&:hover': {
+              backgroundColor: '#343434',
+            },
+          },
+        },
+      ],
+    },
+  },
 });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalProvider>
-      <ThemeProvider theme={theme}>
-        <AuthProvider 
-          authType='cookie'
-          authName='_auth'
-          cookieDomain={window.location.hostname}
-          cookieSecure={window.location.protocol === 'https'}
-        >
-            <BrowserRouter>
-              <App />
-            </BrowserRouter> 
-        </AuthProvider>
-      </ThemeProvider>
-    </GlobalProvider>
+      <GlobalProvider>
+        <ThemeProvider theme={theme}>
+          <AuthProvider 
+            authType='cookie'
+            authName='_auth'
+            cookieDomain={window.location.hostname}
+            cookieSecure={window.location.protocol === 'https'}
+          >
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='en-gb'>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter> 
+            </LocalizationProvider>
+          </AuthProvider>
+        </ThemeProvider>
+      </GlobalProvider>
   </React.StrictMode>,
 )
