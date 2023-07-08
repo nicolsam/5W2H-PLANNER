5b6fa60e6fa172f58af77f61e4c587946d29f28e
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-07 21:10:28 -0300

    feat: routes for goals(planning), actions and responsibles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,23 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import Menu from '@components/Menu';
 
+import Actions from '@pages/Action';
+import EditAction from '@pages/Action/Edit';
+import ShowAction from '@pages/Action/Show';
+import StoreAction from '@pages/Action/Store';
 import Companies from '@pages/Companies';
 import EditCompany from '@pages/Companies/Edit';
 import StoreCompany from '@pages/Companies/Store';
 import Dashboard from '@pages/Dashboard';
 import Error404 from '@pages/Error/404';
+import Goals from '@pages/Goals';
+import EditGoal from '@pages/Goals/Edit';
+import StoreGoal from '@pages/Goals/Store';
 import Login from '@pages/Login';
+import Responsibles from '@pages/Responsibles';
+import EditResponsible from '@pages/Responsibles/Edit';
+import ShowResponsible from '@pages/Responsibles/Show';
+import StoreResponsible from '@pages/Responsibles/Store';
 
 function App() {
 
@@ -65,6 +76,108 @@ function App() {
           element={
             <RequireAuth loginPath="/login">
               <EditCompany />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/planning"
+          element={
+            <RequireAuth loginPath="/login">
+              <Goals />
+            </RequireAuth>
+          }
+          errorElement={<Error404 />}
+        />
+
+        <Route
+          path="/planning/store"
+          element={
+            <RequireAuth loginPath="/login">
+              <StoreGoal />
+            </RequireAuth>
+          }
+        />
+        
+        <Route
+          path="/planning/edit/:goal_id"
+          element={
+            <RequireAuth loginPath="/login">
+              <EditGoal />
+            </RequireAuth>
+          }
+        />
+        
+        <Route
+          path="/action/:goal_id"
+          element={
+            <RequireAuth loginPath="/login">
+              <Actions />
+            </RequireAuth>
+          }
+          errorElement={<Error404 />}
+        />  
+        
+        <Route
+          path="/action/show/:action_id"
+          element={
+            <RequireAuth loginPath="/login">
+              <ShowAction />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/action/store"
+          element={
+            <RequireAuth loginPath="/login">
+              <StoreAction />
+            </RequireAuth>
+          }
+        />
+        
+        <Route
+          path="/action/edit/:action_id"
+          element={
+            <RequireAuth loginPath="/login">
+              <EditAction />
+            </RequireAuth>
+          }
+        />
+        
+        <Route
+          path="/responsibles"
+          element={
+            <RequireAuth loginPath="/login">
+              <Responsibles />
+            </RequireAuth>
+          }
+          errorElement={<Error404 />}
+        />
+      
+        <Route
+          path="/responsibles/:responsible_id"
+          element={
+            <RequireAuth loginPath="/login">
+              <ShowResponsible />
+            </RequireAuth>
+          }
+          errorElement={<Error404 />}
+        />
+        <Route
+          path="/responsibles/store"
+          element={
+            <RequireAuth loginPath="/login">
+              <StoreResponsible />
+            </RequireAuth>
+          }
+        />
+        
+        <Route
+          path="/responsibles/edit/:responsible_id"
+          element={
+            <RequireAuth loginPath="/login">
+              <EditResponsible />
             </RequireAuth>
           }
         />
