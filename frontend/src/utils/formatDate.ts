6ec7cd922b9285c360e8dbb27a7a76cad1f19811
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-12 23:31:19 -0300

    feat: extract date from string and format to Brazil

--- frontend/src/utils/formatDate.ts
+++ frontend/src/utils/formatDate.ts
@@ -10,4 +10,25 @@ export function formatToBrazil(dateString: string) {
     return formattedDate;
 }
 
+export function extractDateAndFormatToBrazil(date: string) {
+
+    if(!date) return
+
+    const extractedDate = date.split('T')[0];
+
+    return convertDateFormat(extractedDate);
+
+
+}
+
+function convertDateFormat(dateString: string) {
+    // Split the input date string into year, month, and day
+    const [year, month, day] = dateString.split('-');
+
+    // Create a new date string in the 'day-month-year' format
+    const newDateString = `${day}/${month}/${year}`;
+
+    return newDateString;
+}
+
 export default formatDate;
