adeef28ce7024ec5ea6246c0a1075c053c3131ab
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-14 00:37:29 -0300

    feat: new item information options The component has now ability to use accordions, show tooltip and use custom badges

--- frontend/src/components/Layout/List/Item/index.tsx
+++ frontend/src/components/Layout/List/Item/index.tsx
@@ -1,7 +1,9 @@
 import Badge from '@components/Badge';
 import { BadgeStatusType } from '@components/Badge/styles';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Button, Divider, IconButton, Menu, MenuItem, Stack } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Alert, AlertTitle, Button, Divider, IconButton, Menu, MenuItem, Stack, Tooltip, Typography } from "@mui/material";
 
 import {
     bindMenu,
@@ -13,10 +15,13 @@ type Props = {
     id: number;
     children: string | JSX.Element;
     description?: string;
+    showCount?: boolean;
+    color: 'primary' | 'secondary';
     click: () => void;
     actions: Action[];
-    firstBadgeSpacing: boolean;
+    firstBadgeSpacing?: boolean;
     badges?: Badge[];
+    accordions?: Accordion[];
 };
 
 type Action = {
@@ -28,63 +33,151 @@ type Action = {
 
 type Badge = {
     name: string,
-    count: number,
+    count?: number,
     status?: BadgeStatusType;
 }
 
-const Item = ({ id, children, description, click, actions, firstBadgeSpacing = false, badges }: Props) => {
+type Accordion = {
+    name: string,
+    content: JSX.Element[] | string;
+    errorMessage: string;
+    errorDescription: string;
+}
+
+const Item = ({ 
+    id, 
+    children, 
+    description, 
+    showCount = false,
+    color,
+    click, 
+    actions, 
+    firstBadgeSpacing = false, 
+    badges,
+    accordions 
+}: Props) => {
     const popupState = usePopupState({
         variant: 'popover',
         popupId: 'options',
     });
 
     return (
-        <div
-            key={id} 
-            className="rounded bg-secondary-color flex flex-row justify-between items-center"
-        >
-            <Button 
-                variant="list" 
-                className="w-full h-full rounded p-4 bg-secondary-color"
-                onClick={click}
-            >
-                <div className="flex flex-col items-start">
-                    <h2 className="text-white text-2xl font-semibold normal-case">{children}</h2>
-                    {description && (
-                        <p className="text-white text-base font-normal normal-case">{description}</p>
-                    )}
-
-                    <Stack sx={{ mt: 1 }} spacing={1} direction={"row"}>
-                    {badges && badges.map((badge: Badge) => (
-                        <Badge firstBadgeSpacing={firstBadgeSpacing} showCount count={badge.count} status={badge.status ? badge.status : 'neutral'}>{badge.name}</Badge>
-                    ))}
-                    </Stack>
-                </div>
-            </Button>
-
-            <Divider orientation="vertical" variant="middle" flexItem />
-
-            <div className="p-4 w-fit">
-                <IconButton {...bindTrigger(popupState)} size="large" sx={{ backgroundColor: '#585858'}}>
-                    <MoreVertIcon className="text-white" fontSize="inherit" />
-                </IconButton>
-                
-                <Menu {...bindMenu(popupState)} >
-                
-                {popupState && actions.map((action, index: number) => (
-                    <MenuItem
-                        key={index}
-                        onClick={action.click}
-                        className="flex gap-2"
+        <Stack direction={"column"} spacing={0} key={id} className="rounded bg-secondary-color" sx={{ backgroundColor: color != "primary" ? '#585858' : "#444344" }}> 
+            <div className="flex flex-row justify-between items-center">
+                <Tooltip 
+                    title={
+                        <div className="2xl:text-lg text-center">
+                            <h2>Clique para visualizar</h2>
+                        </div>
+                    } 
+                    arrow
+                >
+                    <Button
+                        variant="list"
+                        className="w-full h-full rounded p-4 bg-secondary-color"
+                        sx={{ backgroundColor: color != "primary" ? '#585858' : "#444344" }}
+                        onClick={click}
                     >
-                        {action.icon}
-                        {action.name}
-                    </MenuItem>
-                ))}
-                </Menu>
-            </div>
+                        <div className="flex flex-col items-start pt-2">
+                            <h2 className="text-white text-2xl font-semibold normal-case">{children}</h2>
+                            {description && (
+                                <p className="text-white text-base font-normal normal-case">{description}</p>
+                            )}
+                            <Stack sx={{ mt: 2 }} spacing={1} direction={"row"}>
+                            {badges && badges.map((badge: Badge) => (
+                                <Badge firstBadgeSpacing={firstBadgeSpacing} showCount={showCount} count={badge.count} status={badge.status ? badge.status : 'neutral'}>{badge.name}</Badge>
+                            ))}
+                            </Stack>
+                        </div>
+                    </Button>
+                </Tooltip>
 
-        </div>
+                {/* <Divider orientation="vertical" variant="middle" flexItem /> */}
+
+                <div className="p-4 w-fit">
+                    <IconButton 
+                        {...bindTrigger(popupState)} 
+                        size="large" 
+                        sx={{ 
+                            backgroundColor: color != "secondary" ? '#585858' : "#444344",
+                            '&:hover': {
+                                backgroundColor: '#686768',
+                            },
+                        }}
+                    >
+                        <MoreVertIcon className="text-white" fontSize="inherit" />
+                    </IconButton>
+            
+                    <Menu {...bindMenu(popupState)} >
+            
+                    {popupState && actions.map((action, index: number) => (
+                        <MenuItem
+                            key={index}
+                            onClick={action.click}
+                            className="flex gap-2"
+                        >
+                            {action.icon}
+                            {action.name}
+                        </MenuItem>
+                    ))}
+                    </Menu>
+                </div>
+            </div>
+            {accordions && accordions.map((accordion: Accordion, index: number) => (
+                <Accordion 
+                    disableGutters 
+                    elevation={0} 
+                    sx={{ 
+                        backgroundColor:'#444344', 
+                        color: "white", 
+                        paddingTop: '.5rem',
+                        paddingLeft: '1rem',
+                        paddingBottom: '1rem',
+                        flexDirection: 'row' 
+                    }}>
+                    <Stack spacing={1} direction="row">
+                        <AccordionSummary
+                            sx={{
+                                backgroundColor: '#585858',
+                                maxWidth: 'fit-content',
+                                flexDirection: 'row-reverse',
+                                gap: '.5rem',
+                                alignItems: 'center',
+                                paddingRight: '1.4rem',
+                                borderRadius: '0.25rem',
+                                '&:hover': {
+                                    backgroundColor: '#686768',
+                                },
+                            }}
+                            expandIcon={<ExpandMoreIcon style={{ color: 'white' }} />}
+                            aria-controls="panel1a-content"
+                            id={`item-accordion-${index}`}
+                        >
+                            <span className="2xl:text-lg font-normal font-sans">{accordion.name}</span>
+                        </AccordionSummary>
+                        <Button className="h-fit" sx={{padding: 2}} variant="main" startIcon={<AddCircleIcon />} disableElevation onClick={() => {}}>
+                            <span className="2xl:text-base font-normal">ETAPA</span>
+                        </Button>
+                    </Stack>
+                    <AccordionDetails
+                        sx={{
+                            mt: 1,
+                            pl: 0,
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: 1
+                        }}
+                    >
+                        {accordion.content != "" ? accordion.content : (
+                            <Alert severity="warning">
+                                <AlertTitle>{accordion.errorMessage}</AlertTitle>
+                                {accordion.errorDescription}
+                            </Alert>
+                        )}
+                    </AccordionDetails>
+                </Accordion>
+            ))}
+        </Stack>
     )
 }
 
