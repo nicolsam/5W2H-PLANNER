ff1a3ec8e8a83f94f4c0eb330711171733b8a928
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-14 20:34:45 -0300

    feat: better design for 'forbidden' page

--- frontend/src/pages/Error/Forbidden/index.tsx
+++ frontend/src/pages/Error/Forbidden/index.tsx
@@ -1,15 +1,20 @@
-import { Link, useNavigate } from "react-router-dom";
+import ErrorRobot from '@components/Robot/robot';
+import ErrorIcon from '@mui/icons-material/Error';
 
-// TODO Make a better Forbidden error page
 const Forbidden = () => {
 
-    const navigate = useNavigate();
-
     return (
-        <div>
-            <h1>Forbidden</h1>
-
-            <Link to="/company/login">Fazer login</Link>
+        <div className="relative bg-secondary-color w-full h-screen flex flex-col justify-center items-center gap-10">
+            <div className="absolute top-1/4 flex flex-row justify-center items-center gap-7">
+                <ErrorIcon color="error" style={{ fontSize: 80 }} />
+                <div className="flex flex-col">
+                    <h1 className="text-white font-semibold text-4xl lg:text-6xl xl:text-7xl">403</h1>
+                    <p className="text-white text-xl lg:text-2xl xl:text-3xl">Acesso recusado</p>
+                </div>
+            </div>
+            <div className="h-fit absolute top-2/3">
+                <ErrorRobot />
+            </div>
         </div>
 
     )
