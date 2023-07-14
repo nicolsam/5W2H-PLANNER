df4c17092a880a426a8761c71ce102cf183e7ea5
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-07-14 00:28:16 -0300

    type: Stage attributes

---
+++ frontend/src/models/Stage.ts
@@ -0,0 +1,32 @@
+export type StageType = {
+    id: number;
+    action_id: number;
+    attributes: StageAttributes; 
+    count: {
+        stages: {
+            total: number;
+            completed: number;
+            developing: number;
+            start: number
+        }
+    }
+};
+
+export type StageAttributes = {
+    name: string;
+    area: string;
+    what: string;
+    how: string;
+    start_at: string;
+    end_at: string;
+    remaining_days: number;
+    value: string;
+    value_status: 'Solicitar Orçamento' | 'Aguardando Cotação' | 'Orçamento em Apreciação' | 'Aprovado' | 'Não Aprovado' | 'Sem ônus' | 'Não definido';
+    status: 'A Iniciar' | 'Em Andamento' | 'Finalizado';
+    priority: 'Baixa' | 'Média' | 'Alta';
+    observation: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export default StageType;
