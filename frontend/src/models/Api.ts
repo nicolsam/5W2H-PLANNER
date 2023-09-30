4c4903a9f7e64623eab686821850b26b20220358
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-09-29 21:33:17 -0300

    fix: Included actions, stages, responsibles and areas to response type

--- frontend/src/models/Api.ts
+++ frontend/src/models/Api.ts
@@ -1,9 +1,13 @@
+import ActionType from "./Action";
+import AreaType from "./Area";
 import CompanyType from "./Company";
 import GoalType from "./Goal";
+import ResponsibleType from "./Responsible";
+import StageType from "./Stage";
 
 export type ResponseType = {
     success: boolean,
-    data: CompanyType | GoalType | [],
+    data: CompanyType | GoalType | ActionType | StageType | ResponsibleType | AreaType | [],
     message?: string
 }
 
