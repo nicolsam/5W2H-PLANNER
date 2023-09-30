3993371b25f426503250a8f5bdeea70caf4b29d4
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-09-29 21:26:36 -0300

    feat: Area resource

---
+++ backend/app/Http/Resources/AreaResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AreaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at
+            ],
+            'relationships' => [
+                'company' => [
+                    'id' => $this->company->id,
+                    'name' => $this->company->name,
+                    'cnpj' => $this->company->cnpj,
+                    'created_at' => $this->company->created_at,
+                    'updated_at' => $this->company->updated_at
+                ]
+            ]
+        ];
+    }
+}
