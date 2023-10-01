82d693973f1bc343483c027a57e4a23fd5b10739
Nicolas Samuel <nicolas.lorrayne@gmail.com>
2023-10-01 18:29:12 -0300

    fix: better spacing between heading and description. Large minimum button size

--- frontend/src/components/Layout/Header/index.tsx
+++ frontend/src/components/Layout/Header/index.tsx
@@ -21,13 +21,13 @@ const Header = ({ description, storeButton, storeText, redirect, children }: Pro
 
     return (
         <header className="flex justify-between flex-col md:flex-row gap-2 md:gap-4 2xl:gap-0">
-            <Stack spacing={0} direction={"column"} className="md:w-3/4 xl:w-auto">
-                <h1 className="text-4xl xl:text-4xl 2xl:text-5xl font-medium">{children}</h1>
+            <Stack spacing={1} direction={"column"} className="md:w-3/4 xl:w-auto">
+                <h1 className="text-3xl xl:text-4xl 2xl:text-5xl font-medium">{children}</h1>
                 <p className="text-xl mt-1 xl:mt-0 xl:text-xl 2xl:text-2xl">{description}</p>
             </Stack>
             {storeButton && redirect && (
                 
-                <Button className="md:w-1/4 xl:w-auto h-fit" sx={{padding: 2}} variant="main" startIcon={<AddCircleIcon />} disableElevation onClick={navigateToRedirect}>
+                <Button className="md:w-1/4 xl:w-auto h-fit" sx={{ px: 4, py: 2}} variant="main" startIcon={<AddCircleIcon />} disableElevation onClick={navigateToRedirect}>
                     <span className="text-2xl xl:text-xl">{storeText}</span>
                 </Button>
                 
